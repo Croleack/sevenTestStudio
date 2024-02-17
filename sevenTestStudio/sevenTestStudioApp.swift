cbe70bc3bb5cf5329c50a756b14cedd4b3130e41
Radik Khaziev <121854191+Croleack@users.noreply.github.com>
2024-02-17 19:44:57 +0500

    Initial Commit

---
+++ sevenTestStudio/sevenTestStudioApp.swift
@@ -0,0 +1,17 @@
+//
+//  sevenTestStudioApp.swift
+//  sevenTestStudio
+//
+//  Created by Enzhe Gaysina on 17.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct sevenTestStudioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
